b45ede0aa0bce25d1e73b49bff5a9f2d08e0bb49
Qi Chang Lin <mntrlin@gmail.com>
2022-03-04 14:35:20 +0100

    Updated subsampling for PRJNA588788

--- Subsampling.sh
+++ Subsampling.sh
@@ -21,22 +21,22 @@ module load seqtk/1.2-r101
 
 # Unzipping
 echo -e "\n`date` Unzipping"
-gzip -dc ../data/SRR10957229/SRR10957229_1.fastq.gz > $SNIC_TMP/SRR10957229_1.fastq
-gzip -dc ../data/SRR10957229/SRR10957229_2.fastq.gz > $SNIC_TMP/SRR10957229_2.fastq
+gzip -dc SRR10428411/SRR10428411_1.fastq.gz > $SNIC_TMP/SRR10428411_1.fastq
+gzip -dc SRR10428411/SRR10428411_2.fastq.gz > $SNIC_TMP/SRR10428411_2.fastq
 
 # Subsampling with seqtk
 echo -e "\n`date` Subsampling"
-seqtk sample -s747 $SNIC_TMP/SRR10957229_1.fastq 1000000 > SRR10957229_1_sub.fastq
-seqtk sample -s747 $SNIC_TMP/SRR10957229_1.fastq 1000000 > SRR10957229_2_sub.fastq
+seqtk sample -s747 $SNIC_TMP/SRR10428411_1.fastq 1000000 > SRR10428411_1_sub.fastq
+seqtk sample -s747 $SNIC_TMP/SRR10428411_1.fastq 1000000 > SRR10428411_2_sub.fastq
 
 # Deleting raw.fastq
 echo -e "\n`date` Deleting unzipped files"
-rm $SNIC_TMP/SRR10957229_1.fastq
-rm $SNIC_TMP/SRR10957229_2.fastq
+rm $SNIC_TMP/SRR10428411_1.fastq
+rm $SNIC_TMP/SRR10428411_2.fastq
 
 # Gzipping subsamples
 echo -e "\n`date` Gzipping subsamples"
-gzip SRR10957229_1_sub.fastq
-gzip SRR10957229_2_sub.fastq
+gzip SRR10428411_1_sub.fastq
+gzip SRR10428411_2_sub.fastq
 
 echo -e "\n`date` Done with subsampling"
