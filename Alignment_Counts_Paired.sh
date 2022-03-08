90a3e4cae3e4ab9cb8c47d920aa7fa2f5b60b169
Qi Chang Lin <mntrlin@gmail.com>
2022-03-08 11:55:05 +0100

    htseq-count reverse stranded

--- Alignment_Counts_Paired.sh
+++ Alignment_Counts_Paired.sh
@@ -86,7 +86,7 @@ echo -e "\n`date` Counting raw expression values of ${selected_sample} with htse
 mkdir -p ${wd}/processed/${selected_sample}/htseq_count/
 htseq-count -n 16 \
     --order pos \
-    --stranded no \
+    --stranded reverse \
     --counts_output "${wd}/processed/${selected_sample}/htseq_count/${selected_sample}.count" \
     "$SNIC_TMP/processed/${selected_sample}/STAR/${selected_sample}Aligned.sortedByCoord.out.bam" \
     "${ref_gtf}"
