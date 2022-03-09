d27cf906831baca58ea3e6304317c761243264d8
Qi Chang Lin <mntrlin@gmail.com>
2022-03-09 20:59:12 +0100

    Changed to ENA

--- cURL_SRA.sh
+++ cURL_SRA.sh
@@ -18,11 +18,11 @@ cd "${wd}"
 
 # Download
 echo -e "\n`date` Start download"
-curl -L ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR137/SRR13782529/SRR13782529.sra -o SRR13782529_GSM5106143_02_E4_15_H09_Mus_musculus_RNA-Seq.sra
-curl -L ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR137/SRR13782530/SRR13782530.sra -o SRR13782530_GSM5106143_02_E4_15_H09_Mus_musculus_RNA-Seq.sra
-curl -L ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR137/SRR13782531/SRR13782531.sra -o SRR13782531_GSM5106143_02_E4_15_H09_Mus_musculus_RNA-Seq.sra
-curl -L ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR137/SRR13782532/SRR13782532.sra -o SRR13782532_GSM5106144_03_E3_10_H10_Mus_musculus_RNA-Seq.sra
-curl -L ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR137/SRR13782533/SRR13782533.sra -o SRR13782533_GSM5106144_03_E3_10_H10_Mus_musculus_RNA-Seq.sra
-curl -L ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR137/SRR13782534/SRR13782534.sra -o SRR13782534_GSM5106144_03_E3_10_H10_Mus_musculus_RNA-Seq.sra
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR137/029/SRR13782529 -o SRR13782529_GSM5106143_02_E4_15_H09_Mus_musculus_RNA-Seq.sra
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR137/030/SRR13782530 -o SRR13782530_GSM5106143_02_E4_15_H09_Mus_musculus_RNA-Seq.sra
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR137/031/SRR13782531 -o SRR13782531_GSM5106143_02_E4_15_H09_Mus_musculus_RNA-Seq.sra
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR137/032/SRR13782532 -o SRR13782532_GSM5106144_03_E3_10_H10_Mus_musculus_RNA-Seq.sra
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR137/033/SRR13782533 -o SRR13782533_GSM5106144_03_E3_10_H10_Mus_musculus_RNA-Seq.sra
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR137/034/SRR13782534 -o SRR13782534_GSM5106144_03_E3_10_H10_Mus_musculus_RNA-Seq.sra
 
 echo -e "\n`date` Done with download"
