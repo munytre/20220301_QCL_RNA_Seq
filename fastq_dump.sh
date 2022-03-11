ac20d803f76019a3ab62175c7228e565ecd54d09
Qi Chang Lin <mntrlin@gmail.com>
2022-03-11 11:04:12 +0100

    addded fastdump test

---
+++ fastq_dump.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+#SBATCH -A snic2022-22-143
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 72:00:00
+#SBATCH -J fastq_dump
+
+# Error report
+set -euo pipefail
+
+# Starting time
+echo -e "\n`date` Start"
+
+# Working directory
+wd="$1"
+cd "${wd}"
+
+# Download
+echo -e "\n`date` Start download"
+fastq-dump SRR13782529_GSM5106143_02_E4_15_H09_Mus_musculus_RNA-Seq.sra --gzip --split-files -v
+
+echo -e "\n`date` Done with download"
