7db4704c0687cdeb0d8f2228f3656d0d432b8a8d
Qi Chang Lin <mntrlin@gmail.com>
2022-03-22 11:53:36 +0100

    Added dependencies

--- 10X_gen.sh
+++ 10X_gen.sh
@@ -16,6 +16,12 @@ echo -e "\n`date` Start"
 wd="$1"
 cd "${wd}"
 
+# Load required tools
+echo -e "\n`date` Load modules"
+module load bioinfo-tools
+module load samtools/1.14
+module load sratools/2.10.9
+
 # Download
 echo -e "\n`date` Start download"
 sam-dump SRR13782529 | samtools view -bS - > SRR13782529.bam
